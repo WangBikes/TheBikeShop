9ab8154bcf62c1576fa90632b45c7c47637e9956
Xichao <wangx027@richardchalloner.com>
2021-03-25 18:13:10 +0000

    userlogin

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-  return 'test'
+    return render_template("home.html", user=current_user)
+
